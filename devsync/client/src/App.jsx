de0d821cbfee371b361b6ee40839c36a7bd767d3
Kr-Suryansh <krsuryansh05@gmail.com>
2025-11-10 03:03:51 +0530

    File structure done

---
+++ devsync/client/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-linear-to-r from-indigo-500 to-purple-600 text-white">
+      <h1 className="text-5xl font-extrabold mb-4">Tailwind v4 Test 💨</h1>
+      <p className="text-lg">If you see colors and styles — it works!</p>
+      <button className="mt-6 px-6 py-2 bg-white text-indigo-700 font-semibold rounded-lg shadow-md hover:bg-indigo-100">
+        Test Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
